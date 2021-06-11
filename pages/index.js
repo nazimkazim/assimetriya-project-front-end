1326077f7b55deef26fe52f7e2f6ca2fced9e5e1
Nazim <nazik_111@mail.ru>
2021-06-11 18:19:44 +0600

    fix bug

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import styles from '../styles/Home.module.scss';
 import { Wave } from '../sharedComponents/wave';
+import { useRef } from 'react';
 
 import { useEffect, useState } from 'react';
 
 export default function Home() {
   const [mutedVideo, setMuted] = useState(true);
+  const cursorRef = useRef();
+
+  useEffect(() => {
+    function updateScrollPosition() {
+      // update the scroll position
+      console.log('scroll');
+    }
+
+    if (cursorRef && cursorRef.current) {
+      cursorRef.current.addEventListener("mousemove", updateScrollPosition, false);
+      return function cleanup() {
+        cursorRef.current.removeEventListener("mousemove", updateScrollPosition, false);
+      };
+    }
+  }, []);
+
+  console.log(cursorRef);
+
 
   return (
     <div className={ styles.container }>
+      <div ref={ cursorRef } className={ styles.cursor }></div>
       <Wave mutedVideo={ mutedVideo } playVideo={ () => setMuted(!mutedVideo) } />
 
       <video autoPlay={ true } muted={ mutedVideo } loop controls={ false }>
