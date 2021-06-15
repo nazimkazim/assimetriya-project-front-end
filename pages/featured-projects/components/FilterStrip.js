bc34ebe9690cfdceef2bb864348057b444db134f
Nazim <nazik_111@mail.ru>
2021-06-15 19:50:06 +0600

    add fix filter strip export

--- pages/featured-projects/components/FilterStrip.js
+++ pages/featured-projects/components/FilterStrip.js
@@ -1,13 +1,10 @@
 import { Strip, Title, TypesContainer, TypeItem } from '../../../styles/filter.strip.styles';
 
-const FilterStrip = ({ types, setSelectedType, selectedType }) => {
+export const FilterStrip = ({ types, setSelectedType, selectedType }) => {
   return (
     <Strip>
       <Title>Selected Projects</Title>
       <TypesContainer>{ types.map(type => <TypeItem key={ type } chosen={ selectedType === type } onClick={ () => setSelectedType(type) }>{ type }</TypeItem>) }</TypesContainer>
     </Strip>
   );
-
 };
-
-export default FilterStrip;
--- sharedComponents/Header/index.js
+++ sharedComponents/Header/index.js
@@ -17,6 +17,5 @@ export const HeaderComponent = ({ pathName }) => {
         )) }
       </Menu>
     </Content>
-
   );
 };
