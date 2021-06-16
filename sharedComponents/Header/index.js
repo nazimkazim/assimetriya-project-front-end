76912d7f2483acd46a700230729be6f4695d6c7d
Nazim <nazik_111@mail.ru>
2021-06-16 12:28:20 +0600

    add close button for menu and change arrow distance

--- sharedComponents/Header/index.js
+++ sharedComponents/Header/index.js
@@ -2,28 +2,28 @@ import { useState } from 'react';
 import { Content, Logo, Menu, LinkItem } from './styles';
 import { Links } from './Link';
 import Link from 'next/link';
-import { GiHamburgerMenu } from 'react-icons/gi';
 import Media from 'react-media';
 import { MEDIA_QUERY_BREAKPOINTS } from '../../constants';
+import { VscChromeClose, VscMenu } from 'react-icons/vsc';
 
 
 export const HeaderComponent = ({ pathName }) => {
   const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <Content openMenu={ openMenu } pathName={ pathName }>
       <Logo>
         <img src="https://res.cloudinary.com/nzmai/image/upload/v1623471885/assimetriya-project/logo.svg" />
         <Media queries={ { small: `(max-width: ${MEDIA_QUERY_BREAKPOINTS.mobile})` } }>
           { matches =>
-            matches.small ? (
-              <GiHamburgerMenu size={ 30 } onClick={ () => setOpenMenu(!openMenu) }></GiHamburgerMenu>
+            matches.small && !openMenu ? (
+              <VscMenu size={ 30 } onClick={ () => setOpenMenu(!openMenu) } />
             ) : (
-              ''
+              <VscChromeClose size={ 30 } onClick={ () => setOpenMenu(!openMenu) } />
             )
           }
         </Media>
       </Logo>
-
 
       <Menu openMenu={ openMenu } pathName={ pathName }>
         { Links.map(item => (
@@ -31,6 +31,6 @@ export const HeaderComponent = ({ pathName }) => {
         )) }
       </Menu>
 
-    </Content>
+    </Content >
   );
 };
