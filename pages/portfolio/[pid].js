2cd44b646c53e409a0993d205892ca434591bc55
Nazim <nazik_111@mail.ru>
2021-06-21 20:15:15 +0600

    add portfolio

--- pages/portfolio/[pid].js
+++ pages/portfolio/[pid].js
@@ -1,56 +1,70 @@
 import { Container } from "../../styles/featured.projects.styles";
 import { HeaderComponent } from '../../sharedComponents/Header/index';
 import { useRouter } from 'next/router';
-import { homeImages } from '../../home-images';
 import { useEffect, useState } from "react";
 import { ProjectInfoContainer, Image, ProjectName, ProjectLocation } from '../../styles/portfolio.styles';
 import { SocialMediaStrip } from '../../sharedComponents/SocialMediaStrip';
+import { createClient } from 'contentful';
 
-const Portfolio = ({ homeImages }) => {
-  const [projects, setProjects] = useState(homeImages);
+
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY
+  });
+
+  const res = await client.getEntries({ content_type: 'asymetriya' });
+
+  return {
+    props: {
+      c_projects: res.items
+    }
+  };
+}
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: 'blocking' //indicates the type of fallback
+  };
+};
+
+
+const Portfolio = ({ c_projects }) => {
   const [project, setProject] = useState({});
   const router = useRouter();
   const { pid } = router.query;
 
 
   useEffect(() => {
-    setProjects(homeImages);
-  }, [projects]);
-
-  useEffect(() => {
-    console.log(projects);
-    const project = projects.find(project => project.id === pid);
-    console.log(project);
+    const project = c_projects.find(project => project.sys.id === pid);
     setProject(project);
   }, []);
 
   console.log(project);
 
-
-  /* console.log(project);
-  console.log(pid);
-  console.log(homeImages); */
-
   return (
     <Container>
       <HeaderComponent />
       <ProjectInfoContainer>
-        <ProjectName>{ project && project.name }</ProjectName>
-        <ProjectLocation>{ project && project.location }</ProjectLocation>
+        <ProjectName>{ project && project.fields && project.fields.title }</ProjectName>
+        <ProjectLocation>{ project && project.fields && project.fields.address }</ProjectLocation>
         <Image
-          src={ project && project.src }
-          alt={ project && project.name }
+          src={ project && project.fields && project.fields.mainPicture.fields.file.url }
+          alt={ project && project.fields && project.fields.title }
         />
+        { project && project.fields && project.fields.galleries.map(item => (
+          <Image
+            src={ item.fields.file.url }
+            alt={ item.fields.id }
+            key={ item.fields.id }
+          />
+        )) }
         <SocialMediaStrip />
       </ProjectInfoContainer>
     </Container>
   );
-};
-
-Portfolio.getInitialProps = async function () {
-  return {
-    homeImages
-  };
 };
 
 export default Portfolio;
