0a0a1bd97e65191fda8111d65a01b7696ed09328
Nazim <nazik_111@mail.ru>
2021-06-21 20:39:22 +0600

    change filter name

--- sharedComponents/FilterStrip/index.js
+++ sharedComponents/FilterStrip/index.js
@@ -1,11 +1,11 @@
 import { Strip, Title, TypesContainer, TypeItem } from '../../styles/filter.strip.styles.js';
 
 const FilterStrip = ({ types, setSelectedType, selectedType }) => {
-  
+
   return (
     <Strip>
       <Title>Selected Projects</Title>
-      <TypesContainer>{ types.map(type => <TypeItem key={ type } chosen={ selectedType === type } onClick={ () => setSelectedType(type) }>{ type }</TypeItem>) }</TypesContainer>
+      <TypesContainer>{ types.map(type => <TypeItem key={ type } chosen={ selectedType === type } onClick={ () => setSelectedType(type) }>{ type.toUpperCase() }</TypeItem>) }</TypesContainer>
     </Strip>
   );
 };
